2a339f729f9194f33554fd9885965f4c32fc4187
Wendrio F. Melo <wendriofrazao1@gmail.com>
2025-08-16 23:15:11 -0300

    controllers adicionado

---
+++ backend/controllers/authControllers.js
@@ -0,0 +1,31 @@
+const userModel = require('../model/user.js')
+const bcrypt = require('bcrypt');
+
+export const register = async (req, res) => {
+    const { name, email, password, confirmpassword } = req.body
+
+    // validações
+    if (!name) return res.status(412).json({message: "Coloque o nome para registrar"});
+    if (!email) return res.status(412).json({message: "Coloque o email para registrar"});
+    if (!password) return res.status(412).json({message: "Coloque a senha para registrar"}); 
+    if(!confirmpassword) return res.status(412).json({message: "Confirme a senha para registrar"});
+    if(password !== confirmpassword) return res.status(412).json({message: "As senha não coincidem"});
+
+    // verificar se o usuario ja existe
+    const userExist = await userModel.findOne({email});
+
+    if (userExist) return res.status(412).json({message: "Email já existente, coloque outro"});
+
+    try {
+        
+        
+
+    } catch (error) {
+        res.json({sucess: false, message: error.messages})
+    }
+
+}
+
+export const login = async (req, res) => {
+
+}
