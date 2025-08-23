0dc6e42aa236109fb8b6edec593f0e05edb7b33e
Wendrio F. Melo <wendriofrazao1@gmail.com>
2025-08-23 13:13:20 -0300

    Update authMiddleware.js

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -4,19 +4,18 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const authMiddleware = (req, res, next) => {
-const authHeader = req.headers["authorization"];
-  if (!authHeader) {
+const token = req.cookies.token;
+
+  if (!token) {
     return res.status(401).json({ message: "Token não fornecido" });
   }
-
-  const token = authHeader.split(" ")[1]; 
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded; 
     next();
   } catch (err) {
-    return res.status(401).json({ message: "Token inválido" });
+    return res.status(401).json({ message: "Token inválido ou expirado" });
   }
 }
 
