9a2e3395399d2f8c02044839cfe2276193f483f6
Wendrio F. Melo <wendriofrazao1@gmail.com>
2025-08-27 11:34:20 -0300

    Update authControllers.js

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -72,7 +72,7 @@ const register = async (req, res) => {
         }
 
      transporter.sendMail(emailOption, (err, info) => {
-        err ? console.error("Erro ao enviar email:", err) :  console.log("Email enviado:", info.response)
+        err ? console.error("Erro ao enviar email:", err) : console.log("Email enviado:", info.response)
      });
 
         console.log("Usuário cadastrado com sucesso!");
