a1ffd522f569a1898afe1b038ad2b61751a973ff
Wendrio F. Melo <wendriofrazao1@gmail.com>
2025-11-20 12:03:04 -0300

    remoção de alguns arquivos

--- backend/routes/userRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const userRouter = express.Router();
-const authMiddleware = require('../middlewares/authMiddleware.js');
-const getUserData = require('../controllers/userController.js');
-
-
-userRouter.get('data', authMiddleware ,getUserData);
-
-module.exports = userRouter;
